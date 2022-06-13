2bcc3180afa93c931e6eaac77baa825a63b1a63c
Aditya <adikpote@gmail.com>
2022-06-13 14:05:01 +0530

    major

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./containers/App";
-import "tachyons";
-import "./index.css";
-import ErrorBoundary from "./components/ErrorBoundary";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <ErrorBoundary>
-            <App />
-        </ErrorBoundary>
-    </React.StrictMode>
-);
 
+ReactDOM.render( <App/> ,document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+
+//React uses JSX to create their virtual DOM
